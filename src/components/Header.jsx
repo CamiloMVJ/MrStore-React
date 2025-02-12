ce68f0f6dbbc64d26dc933f3d942c460f13a2ea1
Camilo <110619742+CamiloMVJ@users.noreply.github.com>
2025-02-11 22:33:18 -0600

    'update'

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div className="navigation">
+            <div className="nav-center container d-flex">
+                <a href="" className="logo"></a>
+                <ul className="nav-list d-flex"></ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header
