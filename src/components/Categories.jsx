03707ac3f59ae295162a7f6cd95d384a89888945
Camilo <110619742+CamiloMVJ@users.noreply.github.com>
2025-02-17 11:00:26 -0600

    'update'

---
+++ src/components/Categories.jsx
@@ -0,0 +1,30 @@
+const Categories = () => {
+    return (
+        <>
+            <section class="section category">
+                <div class="cat-center">
+                    <div class="cat">
+                        <img src="https://th.bing.com/th/id/OIP.hc5ryGVwPtP0Jo3v79biMAHaJh?rs=1&pid=ImgDetMain" alt="" />
+                        <div>
+                            <p>MUJERES</p>
+                        </div>
+                    </div>
+                    <div class="cat">
+                        <img src="https://ieozatljukwgdhkwfyyz.supabase.co/storage/v1/object/public/Images//acce.jpeg" alt="" />
+                        <div>
+                            <p>ACCESORIOS</p>
+                        </div>
+                    </div>
+                    <div class="cat">
+                        <img src="https://revistavelvet.cl/wp-content/uploads/2021/12/TCF-001.jpg" alt="" />
+                        <div>
+                            <p>HOMBRES</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Categories
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../Header"
 import Glider from "../Glider"
+import Categories from "../Categories"
 
 const Home = () => {
     const navSections = [{ title: "Inicio" }, { title: "Tienda" }, { title: "Pedidos" }, { title: "Inventario" }]
@@ -8,6 +9,7 @@ const Home = () => {
             <div className='top-nav hei'></div>
             <Header navSection={navSections}/>
             <Glider />
+            <Categories></Categories>
         </>
     )
 }
