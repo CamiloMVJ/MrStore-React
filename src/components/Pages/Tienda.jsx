cccf8ed65bfdc58f4274043c135e2c80e1da8b5c
paou03 <adriulloa06@gmail.com>
2025-05-13 23:33:41 -0600

    'update'

--- src/components/Pages/Tienda.jsx
+++ src/components/Pages/Tienda.jsx
@@ -13,7 +13,8 @@ const Tienda = () => {
     const [filtro, setFiltro] = useState([])
     const [presetCat, setPresetCats] = useState(useParams().categoria)
     const capitalizar = (str) => str.charAt(0).toUpperCase() + str.slice(1);
-
+  
+    
     useEffect(() => {
         const fetchCategories = async () => {
             const { data } = await supabase.from('categorias').select('nombre_categoria, id_categoria')
