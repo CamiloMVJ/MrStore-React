9041cb0743f6a5c744dee2596f88b17c378ccf0b
harleemsolorzano19 <harleem.solorzano11u@std.uni.edu.ni>
2025-06-23 21:04:44 -0600

    'update'

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -2,7 +2,7 @@ import Header from "../Header"
 import Glider from "../Glider"
 import { useState, useEffect } from "react";
 import { getTable } from "../../js/supabase"
-import ProductsCatalog from "../ProductsCatalog";
+import Products from "../Products";
 import Footer from "../Footer";
 
 const Home = () => {
@@ -68,7 +68,7 @@ const Home = () => {
                     <h1>NUESTROS PRODUCTOS</h1>
                     <p>Calidad a tus manos</p>
                 </div>
-                <ProductsCatalog productos={products} />
+                <Products productos={products} />
             </section>
             <Footer />
         </>
